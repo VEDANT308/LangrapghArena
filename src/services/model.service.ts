15d86be64a377c6aa7793de49b0ed9f51472969d
Vedant Sahare <vsahare484@gmail.com>
2026-06-04 01:32:35 +0530

    feat: update dependencies and refactor graph service for improved state management

--- src/services/model.service.ts
+++ src/services/model.service.ts
@@ -1,20 +1,22 @@
-import { ChatGoogle } from "@langchain/google";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { ChatMistralAI } from "@langchain/mistralai";
-import { ChatCohere } from "@langchain/cohere"
+import { ChatCohere } from "@langchain/cohere";
+import config from "../config/config.js";
 
-import getconfig from "../config/config.js";
 
-export const GeminiModel = new ChatGoogle({
-    model: "gemini-2.5-flash",
-    apiKey: getconfig.GOOGLE_API_KEY,
-});
 
-export const MistralModel = new ChatMistralAI({
+export const geminiModel = new ChatGoogleGenerativeAI({
+    model: "gemini-1.5-flash",
+    apiKey: config.GOOGLE_API_KEY,
+})
+
+export const mistralAIModel = new ChatMistralAI({
     model: "mistral-medium-latest",
-    apiKey: getconfig.MISTRAL_API_KEY,
-});
+    apiKey: config.MISTRAL_API_KEY,
+})
 
-export const CohereModel = new ChatCohere({
+
+export const cohereModel = new ChatCohere({
     model: "command-a-03-2025",
-    apiKey: getconfig.COHERE_API_KEY,
-});
+    apiKey: config.COHERE_API_KEY,
+})
